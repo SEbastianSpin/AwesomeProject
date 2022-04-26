9bd8bbf6a3ec03a00839a6851bc86f08d272ad5c
SEbastianSpin <sebasespinalejo@gmail.com>
2022-04-26 16:53:09 +0200

    goal app , layout  + goal useState Hook

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View,Button, TextInput } from 'react-native';
 
 export default function App() {
+const [enteredGoalText,setenteredGoalText]=useState("");
+
+  function goalInputH(enteredText){
+    setenteredGoalText(enteredText);
+  }
+
+  function addButtonH(){
+    console.log(enteredGoalText);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appcontainer}>
+    <View style={styles.GoalBox}>
+    <TextInput style={styles.Goalinput} placeholder="Whats your goal" onChange={goalInputH}/>
+      <Button title="Add"  onPress={addButtonH} />
+    </View>
+
+    <View  style={styles.GoalsBox}>
+      <Text>List of goals</Text>
+    </View>
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appcontainer: {
+    flex:1,
+    padding:10
   },
+
+  GoalBox: {
+   flexDirection:'row',
+   alignItems:'center',
+   justifyContent:'space-between',
+   flex: 2
+  },
+  GoalsBox: {
+    flexDirection:'row',
+    justifyContent:'space-between',
+    flex: 7
+   },
+
+  Goalinput: {
+    borderColor:'#DDDDDD',
+    borderWidth:3,
+    width: '75%'
+   },
+
 });
